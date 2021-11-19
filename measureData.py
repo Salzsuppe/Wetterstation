7bf7068b5c4e4e911ed5fed58f227a7b368af35a
Salzsuppe <tlwfunky@gmail.com>
2021-11-19 23:52:57 +0100

    temperature menu

--- measureData.py
+++ measureData.py
@@ -63,6 +63,7 @@ def main():
         measuredValueResult = measureValues()
     except:
         measuredValueResult = ["Nice and fake data", 0, 1, 2, 3, 4, 5, 6, 7, 8]
+        print(measuredValueResult)
     return measuredValueResult
 
 # Prevent execution on import (It just works.)
--- static/main.js
+++ static/main.js
@@ -4,7 +4,7 @@ function getRandomArbitrary(min, max) {
 function makeRain(){
     var raindropWrapper = document.querySelector(".raindropWrapper")
     for (i = 0; i<160; i++){
-        var raindrop = '<div class="raindrop" style="top:'+getRandomArbitrary(0, 100)+'%; left:'+getRandomArbitrary(0, 100)+'%;"></div>'
+        var raindrop = '<div class="raindrop" style="top:'+getRandomArbitrary(0, 99)+'%; left:'+getRandomArbitrary(0, 100)+'%;"></div>'
         raindropWrapper.insertAdjacentHTML('beforeend', raindrop)
     }
 }
@@ -15,24 +15,44 @@ function changeTemp(temp, unit) {
     }
 }
 
+let menuActivated = false;
+
+function showMenu() {
+    if (!menuActivated) {
+        for (const el of document.querySelectorAll(".menu-pulldown")) {
+            el.classList.add("menu-pulldown-activated")
+        }
+        menuActivated = true;
+    }
+    else {
+        for (const el of document.querySelectorAll(".menu-pulldown")) {
+            el.classList.remove("menu-pulldown-activated")
+        }
+        menuActivated = false;
+    }
+}
+
 let weatherData = null;
 
 async function fillValues(){
     let response = await fetch("/getdata");
     weatherData = await response.json();
-    changeTemp(weatherData.tmpC, "C")
+    changeTemp(weatherData['curr-0h']['TemperatureC'], "C")
 }
 
 function onClickCelsius() {
-    changeTemp(weatherData.tmpC, "C")
+    changeTemp(weatherData['curr-0h']['TemperatureC'], "C")
+    showMenu()
 }
 
 function onClickFahrenheit() {
-    changeTemp(weatherData.tmpF, "F")
+    changeTemp(weatherData['curr-0h']['TemperatureF'], "F")
+    showMenu()
 }
 
 function onClickKelvin() {
-    changeTemp(weatherData.tmpK, "K")
+    changeTemp(weatherData['curr-0h']['TemperatureK'], "K")
+    showMenu()
 }
 
 function changeSun(){
@@ -49,6 +69,10 @@ function changeRain(){
     document.querySelector(".rainy").style.display = "flex"
     document.querySelector(".sunny").style.display = "none"
     document.querySelector(".cloudy").style.display = "none"
+}
+
+for (const el of document.querySelectorAll(".menu-button")) {
+    el.addEventListener("click", ()=>{showMenu()});
 }
 
 fillValues()
