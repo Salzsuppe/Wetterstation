6e2ee7ff5656382b88d6a442dae3527ad57d3439
Salzsuppe <tlwfunky@gmail.com>
2021-11-20 01:09:14 +0100

    comments

--- static/main.js
+++ static/main.js
@@ -2,6 +2,7 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 function makeRain(){
+    //creates new divs which are randomly shown on the screen
     var raindropWrapper = document.querySelector(".raindropWrapper")
     for (i = 0; i<160; i++){
         var raindrop = '<div class="raindrop" style="top:'+getRandomArbitrary(0, 99)+'%; left:'+getRandomArbitrary(0, 100)+'%;"></div>'
@@ -10,6 +11,7 @@ function makeRain(){
 }
 
 function changeTemp(temp, unit) {
+    //gets every .temp element and changes the text in it
     for (const el of document.querySelectorAll(".temp")) {
         el.innerText = temp + "°" + unit
     }
@@ -18,6 +20,7 @@ function changeTemp(temp, unit) {
 let menuActivated = false;
 
 function showMenu() {
+    //activates the menu for changing the temperature units
     if (!menuActivated) {
         for (const el of document.querySelectorAll(".menu-pulldown")) {
             el.classList.add("menu-pulldown-activated")
@@ -35,11 +38,13 @@ function showMenu() {
 let weatherData = null;
 
 async function fillValues(){
+    //gets all the data from the flask application in app.py
     let response = await fetch("/getdata");
     weatherData = await response.json();
     changeTemp(weatherData['curr-0h']['TemperatureC'], "C")
 }
 
+//changing the temperature values on button click
 function onClickCelsius() {
     changeTemp(weatherData['curr-0h']['TemperatureC'], "C")
     showMenu()
@@ -71,6 +76,7 @@ function changeRain(){
     document.querySelector(".cloudy").style.display = "none"
 }
 
+//adds an onclick event for the menu buttons
 for (const el of document.querySelectorAll(".menu-button")) {
     el.addEventListener("click", ()=>{showMenu()});
 }
