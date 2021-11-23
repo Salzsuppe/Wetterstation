12ea4e08404f2ce841c5f9eafe11a0448364da28
Salzsuppe <tlwfunky@gmail.com>
2021-11-23 11:31:41 +0100

    changes

--- static/main.js
+++ static/main.js
@@ -5,7 +5,7 @@ function makeRain(){
     //creates new divs which are randomly shown on the screen
     var raindropWrapper = document.querySelector(".raindropWrapper")
     for (i = 0; i<160; i++){
-        var raindrop = '<div class="raindrop" style="top:'+getRandomArbitrary(0, 99)+'%; left:'+getRandomArbitrary(0, 100)+'%;"></div>'
+        var raindrop = '<div class="raindrop" style="top:'+getRandomArbitrary(0, 99)+'%; left:'+getRandomArbitrary(0, 99)+'%;"></div>'
         raindropWrapper.insertAdjacentHTML('beforeend', raindrop)
     }
 }
@@ -17,30 +17,12 @@ function changeTemp(temp, unit) {
     }
 }
 
-let menuActivated = false;
-
-function showMenu() {
-    //activates the menu for changing the temperature units
-    if (!menuActivated) {
-        for (const el of document.querySelectorAll(".menu-pulldown")) {
-            el.classList.add("menu-pulldown-activated") //changes the class name to show stmh else wich is styled in css
-        }
-        menuActivated = true;
-    }
-    else {
-        for (const el of document.querySelectorAll(".menu-pulldown")) {
-            el.classList.remove("menu-pulldown-activated") //changes the class name again to show what it was before
-        }
-        menuActivated = false;
-    }
-}
-
 let weatherData = null;
 
 async function fillValues(){
     //gets all the data from the flask application in app.py
     let response = await fetch("/getdata");
-    weatherData = await response.json();
+    weatherData.toFixed(2) = await response.json();
     changeTemp(weatherData['curr-0h']['TemperatureC'], "C")
 }
 
@@ -57,6 +39,30 @@ function onClickKelvin() {
     changeTemp(weatherData['curr-0h']['TemperatureK'], "K")
     showMenu()
 }
+
+let menuActivated = false;
+
+function showMenu() {
+    //activates the menu for changing the temperature units
+    if (!menuActivated) {
+        for (const el of document.querySelectorAll(".menu-pulldown")) {
+            el.classList.add("menu-pulldown-activated") //changes the class name to show smth else wich is styled in css
+        }
+        menuActivated = true;
+    }
+    else {
+        for (const el of document.querySelectorAll(".menu-pulldown")) {
+            el.classList.remove("menu-pulldown-activated") //changes the class name again to show what it was before
+        }
+        menuActivated = false;
+    }
+}
+
+//adds an onclick event for the menu buttons
+for (const el of document.querySelectorAll(".menu-button")) {
+    el.addEventListener("click", ()=>{showMenu()});
+}
+
 //calculate felt temperature
 function getFeelingTemp() {
     var t = weatherData['curr-0h']['TemperatureC']
@@ -65,15 +71,13 @@ function getFeelingTemp() {
     vSix = Math.pow(wind, 0.16) //function for calculating with exponents
 
     var feeling = 13.12 + 0.6215*t - 11.37*vSix + 0.3965*t*vSix
+    var roundFeeling = feeling.toFixed(2)
 
-    changeTemp(feeling)
+
+    changeTemp(roundFeeling)
     showMenu()
 }
 
-//adds an onclick event for the menu buttons
-for (const el of document.querySelectorAll(".menu-button")) {
-    el.addEventListener("click", ()=>{showMenu()});
-}
 
 fillValues()
 makeRain()
